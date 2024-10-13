dd2252b1dc9fb1ffeeb8cf79bf332d1d991d72da
mehedipro3 <mehediratul37@gmail.com>
2024-10-13 21:49:40 +0600

    all done

--- js/video.js
+++ js/video.js
@@ -45,8 +45,8 @@ const displayDetails =(video) =>{
   document.getElementById('customModal').showModal();
 }
 // create loadVideos
-const loadVideos = () => {
-  fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+const loadVideos = (searchText = "") => {
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then(res => res.json())
     .then(data => displayVideos(data.videos))
     .catch(error => console.log(error))
@@ -164,7 +164,9 @@ const displayCategories = (categories) => {
   });
 };
 
-
+document.getElementById('search-input').addEventListener('keyup',(e)=>{
+  loadVideos(e.target.value)
+})
 
 
 
